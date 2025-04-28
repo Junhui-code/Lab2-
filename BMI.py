273bb2f5989b37723634d069e13d4da72d9a0961
alalajkl <wangjunhui52121314@gmail.com>
2025-04-28 15:19:17 +0800

    BMI added

--- BMI.py
+++ BMI.py
@@ -11,3 +11,9 @@ Weight = float(input("Enter your weight in kilograms: "))
 Height = float(input("Enter your height in meters: "))
 bmi = calculate_bmi(Weight, Height)
 print("Your BMI is:", bmi)
+if (bmi < 18.5):
+    print("You are underweight.")
+elif (bmi >= 18.5 and bmi <= 24.9):
+    print("You are in the normal weight range.")
+elif (bmi >= 25):
+    print("You are overweight.")
