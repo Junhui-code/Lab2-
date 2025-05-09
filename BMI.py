feda9d3db47163f2bdccec64ad365fb5227bdab6
alalajkl <wangjunhui52121314@gmail.com>
2025-05-09 14:30:53 +0800

    BMI edited, now it returns -1, 1, 0

--- BMI.py
+++ BMI.py
@@ -2,8 +2,7 @@ def calculate_bmi(weight, height):
     
     if weight <= 0 or height <= 0:
         return "Invalid input. Weight and height must be positive values."
-    print("Height =  ",height)
-    print("Weight =  ",weight)
+
     bmi = weight / (height ** 2)
 
     if bmi < 18.5:
@@ -13,13 +12,3 @@ def calculate_bmi(weight, height):
     elif bmi >= 25:
         return 1
 
-Weight = float(input("Enter your weight in kilograms: "))
-Height = float(input("Enter your height in meters: "))
-bmi = calculate_bmi(Weight, Height)
-print("Your BMI is:", bmi)
-if (bmi < 18.5):
-    print("You are underweight.")
-elif (bmi >= 18.5 and bmi <= 24.9):
-    print("You are in the normal weight range.")
-elif (bmi >= 25):
-    print("You are overweight.")
